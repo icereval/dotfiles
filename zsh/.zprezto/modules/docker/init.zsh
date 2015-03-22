427492e8022d56e668b0af935a19892a90e3d0ec
icereval <icereval@gmail.com>
2015-03-22 15:23:14 -0400

    google-cloud-sdk, cleanup boot2docker init

--- zsh/.zprezto/modules/docker/init.zsh
+++ zsh/.zprezto/modules/docker/init.zsh
@@ -3,9 +3,7 @@
 #
 
 # Boot2Docker
-export DOCKER_HOST=tcp://192.168.59.103:2376
-export DOCKER_CERT_PATH=$HOME/.boot2docker/certs/boot2docker-vm
-export DOCKER_TLS_VERIFY=1
+$(docker-machine env dev)
 
 #
 # Aliases
---
+++ zsh/.zprezto/modules/google/init.zsh
@@ -0,0 +1,11 @@
+#
+# Google
+#
+
+# Google Cloud SDK
+#
+# The next line updates PATH for the Google Cloud SDK.
+source '/Users/michael/google-cloud-sdk/path.zsh.inc'
+
+# The next line enables bash completion for gcloud.
+source '/Users/michael/google-cloud-sdk/completion.zsh.inc'
