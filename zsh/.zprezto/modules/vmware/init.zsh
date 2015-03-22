d5050ebff84d26844a3b17c403b499cfd9f00851
icereval <icereval@gmail.com>
2015-03-22 15:47:45 -0400

    vmware nat helper alias

--- zsh/.zprezto/modules/vmware/init.zsh
+++ zsh/.zprezto/modules/vmware/init.zsh
@@ -7,3 +7,5 @@
 #
 
 alias vmnet-cli="sudo /Applications/VMware\ Fusion.app/Contents/Library/vmnet-cli"
+alias vmnet-restart="sudo /Applications/VMware\ Fusion.app/Contents/Library/vmnet-cli --stop && sudo /Applications/VMware\ Fusion.app/Contents/Library/vmnet-cli --start"
+alias vmnet-nat="sudo vim /Library/Preferences/VMware\ Fusion/vmnet8/nat.conf"
