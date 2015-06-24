ce2c27f50705dcd9634ab8ccc8601f6634852551
icereval <icereval@gmail.com>
2015-06-24 09:15:35 -0400

    fix docker shortcuts

--- zsh/.zprezto/modules/docker/init.zsh
+++ zsh/.zprezto/modules/docker/init.zsh
@@ -13,10 +13,10 @@ $(boot2docker shellinit)
 
 alias dsa='docker stop $(docker ps -aq)'
 alias drma='docker rm -f $(docker ps -aq)'
-alias drmin='docker rmi -f $(docker images --no-trunc | grep "^<none>" | awk "{print $3}")'
+alias drmin='docker rmi -f $(docker images --no-trunc | grep "^<none>" | awk "{print \$3}")'
 
 #
 # Functions
 #
 
-function drmi() { docker rmi -f $(docker images --no-trunc | grep -v "^REPOSITORY.*TAG" | grep "^$1" | awk "{print $3}") }
+function drmi() { docker rmi -f $(docker images --no-trunc | grep -v "^REPOSITORY.*TAG" | grep "^$1" | awk "{print \$3}") }
