e12705d7c0c27dd872e132b972507663b0dfde37
icereval <icereval@gmail.com>
2016-08-02 11:57:52 -0400

    more

--- zsh/.zprezto/modules/google-cloud-sdk/init.zsh
+++ zsh/.zprezto/modules/google-cloud-sdk/init.zsh
@@ -3,6 +3,6 @@
 #
 
 # google-cloud-sdk is installed at /opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk. Add your profile:
-source '/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
-source '/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
+source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
+source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
 
