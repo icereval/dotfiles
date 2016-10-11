8d6c26df0da91617af697dbabb0b1501f0bcf95d
icereval <icereval@gmail.com>
2016-10-10 23:30:01 -0400

    git grog, exclude .autoenv files

---
+++ zsh/.zprezto/modules/git/init.zsh
@@ -0,0 +1,9 @@
+#
+# Git
+#
+
+#
+# Aliases
+#
+git config --global alias.grog 'log --graph --abbrev-commit --decorate --format=format:"%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(dim white) - %an%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n %C(white)%s%C(reset)"'
+
