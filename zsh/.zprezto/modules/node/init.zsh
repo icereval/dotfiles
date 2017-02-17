dfa6e1509832b789b185f3cda440ddc8f16de257
icereval <icereval@gmail.com>
2017-02-17 17:11:07 -0500

    gpg for commit signing

--- zsh/.zprezto/modules/node/init.zsh
+++ zsh/.zprezto/modules/node/init.zsh
@@ -5,7 +5,7 @@
 # Source Node Version Manager
 # https://github.com/Homebrew/homebrew/blob/master/Library/Formula/nvm.rb
 export NVM_DIR=~/.nvm
-source $(brew --prefix nvm)/nvm.sh
+#source $(brew --prefix nvm)/nvm.sh
 
 # Set NVM default as user default
 #ln -sf `which node` /usr/local/bin/node
